843ecbfaf614f5547317e2c588cc4eead94df787
Juan Camilo Marin Navia <jcmnavia@hotmail.com>
2022-03-28 10:57:35 -0500

    fix(contact): Changed destination email

--- web/src/components/contact.js
+++ web/src/components/contact.js
@@ -1,218 +1,233 @@
 import React from "react";
 import CTALink from "./CTALink";
-import '../styles/contact.css';
+import "../styles/contact.css";
 
 import "bootstrap/dist/css/bootstrap.css";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
+import { Form } from "react-bootstrap";
+import ReactDOM from "react-dom";
 
-import { Form } from 'react-bootstrap';
-import ReactDOM from 'react-dom'
+import { useFormik } from "formik";
+import * as yup from "yup";
 
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-
-const Contact = (props) => {
-    const lang = props.lang;
-  console.log("Contact props ",props);
+const Contact = props => {
+  const lang = props.lang;
+  console.log("Contact props ", props);
 
   const StyleBackground = {
-    backgroundColor: props.backgroundColor.hex,
-  }
-
+    backgroundColor: props.backgroundColor.hex
+  };
 
   const requiredText = "This field is required";
 
   const validations = yup.object().shape({
-      name: yup.string().required(requiredText),
-      email: yup.string().email().required(requiredText),
-      company: yup.string().required(requiredText),
-      message: yup.string().required(requiredText),
-      interest: yup.string().required(requiredText),
+    name: yup.string().required(requiredText),
+    email: yup
+      .string()
+      .email()
+      .required(requiredText),
+    company: yup.string().required(requiredText),
+    message: yup.string().required(requiredText),
+    interest: yup.string().required(requiredText)
   });
-
 
   const formik = useFormik({
-      initialValues: {
-          name: '',
-          email: '',
-          company: '',
-          message: '',
-          interest: '',
-      },
-      validationSchema: validations,
-      onSubmit: values => {
-          
-          var myHeaders = new Headers();
-          myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    initialValues: {
+      name: "",
+      email: "",
+      company: "",
+      message: "",
+      interest: ""
+    },
+    validationSchema: validations,
+    onSubmit: values => {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-          var urlencoded = new URLSearchParams();
-          urlencoded.append("name", document.getElementById("name").value);
-          urlencoded.append("email", document.getElementById("email").value);
-          urlencoded.append("company", document.getElementById("company").value);
-          urlencoded.append("message", document.getElementById("message").value);
-          urlencoded.append("to", document.getElementById("to").value);
-          urlencoded.append("site", document.getElementById("site").value);
-          urlencoded.append("interest", document.getElementById("interest").value);
+      var urlencoded = new URLSearchParams();
+      urlencoded.append("name", document.getElementById("name").value);
+      urlencoded.append("email", document.getElementById("email").value);
+      urlencoded.append("company", document.getElementById("company").value);
+      urlencoded.append("message", document.getElementById("message").value);
+      urlencoded.append("to", document.getElementById("to").value);
+      urlencoded.append("site", document.getElementById("site").value);
+      urlencoded.append("interest", document.getElementById("interest").value);
 
-          var requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          body: urlencoded,
-          redirect: 'follow'
-          };
+      var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: "follow"
+      };
 
-          fetch("https://l6kg4l50gi.execute-api.us-east-1.amazonaws.com/send-forms", requestOptions)
-          .then(response => response.text())
-          .then((result) => {
-              const rootElement = document.getElementById('output');
-              const element = React.createElement(
-                  'div',
-                  { className: 'alert alert-info' },
-                  'Message Sent!'
-              );
-              formik.resetForm();
-              ReactDOM.hydrate(element, rootElement);
-          }, (error) => {
-              const rootElement = document.getElementById('output');
-              const element = React.createElement(
-                  'div',
-                  { className: 'alert alert-warning' },
-                  'Message unsent, please try again'
-              );
-              ReactDOM.hydrate(element, rootElement);
-          })
-          .catch(error => console.log('error', error));
-
-          
-
-      },
+      fetch("https://l6kg4l50gi.execute-api.us-east-1.amazonaws.com/send-forms", requestOptions)
+        .then(response => response.text())
+        .then(
+          result => {
+            const rootElement = document.getElementById("output");
+            const element = React.createElement(
+              "div",
+              { className: "alert alert-info" },
+              "Message Sent!"
+            );
+            formik.resetForm();
+            ReactDOM.hydrate(element, rootElement);
+          },
+          error => {
+            const rootElement = document.getElementById("output");
+            const element = React.createElement(
+              "div",
+              { className: "alert alert-warning" },
+              "Message unsent, please try again"
+            );
+            ReactDOM.hydrate(element, rootElement);
+          }
+        )
+        .catch(error => console.log("error", error));
+    }
   });
-  
+
   return (
     <section className="Contactblock" id={props.id} style={StyleBackground}>
       <div class="container">
-          <Row>
-              <Col md={6}>
-                <h2>
-                    <span className="partA">{props.titlePartA[lang]}</span><br/>
-                    <span className="partB">{props.titlePartB[lang]}</span>
-                    <span className="partC">{props.titlePartC[lang]}</span><br/>
-                    <span className="partD">{props.titlePartD[lang]}</span>
-                </h2>
-                <p>{props.pargraph[lang]}</p>
-              </Col>
-              <Col md={6}>
-                  <form className="contact-form" id="myform" onSubmit={formik.handleSubmit}>
-                    <Form.Group><Form.Control type="hidden" name="to" id="to"  value="info@hitexperts.com" /></Form.Group>
-                    <Form.Group><Form.Control type="hidden" name="site" id="site"  value="https://www.2centric.com/" /></Form.Group>
-                        
-                    <Row>
-                        <Col md={6}>
-                              <Form.Group>
-                              <Form.Control type="text"
-                                  name="name"
-                                  id="name"
-                                  className="form-control formfield smallw"
-                                  placeholder={props.labelName[lang]}
-                                  onChange={formik.handleChange}
-                                  value={formik.values.name}
-                                  isInvalid={!!formik.errors.name} />
+        <Row>
+          <Col md={6}>
+            <h2>
+              <span className="partA">{props.titlePartA[lang]}</span>
+              <br />
+              <span className="partB">{props.titlePartB[lang]}</span>
+              <span className="partC">{props.titlePartC[lang]}</span>
+              <br />
+              <span className="partD">{props.titlePartD[lang]}</span>
+            </h2>
+            <p>{props.pargraph[lang]}</p>
+          </Col>
+          <Col md={6}>
+            <form className="contact-form" id="myform" onSubmit={formik.handleSubmit}>
+              <Form.Group>
+                <Form.Control type="hidden" name="to" id="to" value="info@2centric.com" />
+              </Form.Group>
+              <Form.Group>
+                <Form.Control
+                  type="hidden"
+                  name="site"
+                  id="site"
+                  value="https://www.2centric.com/"
+                />
+              </Form.Group>
 
-                              <Form.Control.Feedback type="invalid">
-                                  {formik.errors.name}
-                              </Form.Control.Feedback>
+              <Row>
+                <Col md={6}>
+                  <Form.Group>
+                    <Form.Control
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="form-control formfield smallw"
+                      placeholder={props.labelName[lang]}
+                      onChange={formik.handleChange}
+                      value={formik.values.name}
+                      isInvalid={!!formik.errors.name}
+                    />
 
-                          </Form.Group>
-                        </Col>
-                        <Col md={6}>
+                    <Form.Control.Feedback type="invalid">
+                      {formik.errors.name}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group>
+                    <Form.Control
+                      type="email"
+                      className="form-control formfield"
+                      placeholder={props.labelEmail[lang]}
+                      name="email"
+                      id="email"
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                      isInvalid={!!formik.errors.email && formik.touched.email}
+                    />
 
-                           <Form.Group>
-                                <Form.Control
-                                    type="email"
-                                    className="form-control formfield"
-                                    placeholder={props.labelEmail[lang]}
-                                    name="email"
-                                    id="email"
-                                    onChange={formik.handleChange}
-                                    value={formik.values.email}
-                                    isInvalid={!!formik.errors.email && formik.touched.email} />
+                    <Form.Control.Feedback type="invalid">
+                      {formik.errors.email}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={6}>
+                  <Form.Group>
+                    <Form.Control
+                      type="text"
+                      className="form-control formfield"
+                      placeholder={props.labelCompany[lang]}
+                      name="company"
+                      id="company"
+                      onChange={formik.handleChange}
+                      value={formik.values.company}
+                      isInvalid={!!formik.errors.company && formik.touched.company}
+                    />
 
-                                <Form.Control.Feedback type="invalid">
-                                    {formik.errors.email}
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md={6}>
-                          <Form.Group>
-                                <Form.Control
-                                    type="text"
-                                    className="form-control formfield"
-                                    placeholder={props.labelCompany[lang]}
-                                    name="company"
-                                    id="company"
-                                    onChange={formik.handleChange}
-                                    value={formik.values.company}
-                                    isInvalid={!!formik.errors.company && formik.touched.company} />
+                    <Form.Control.Feedback type="invalid">
+                      {formik.errors.company}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group>
+                    <Form.Control
+                      type="text"
+                      className="form-control formfield"
+                      placeholder={props.labelInterest[lang]}
+                      name="interest"
+                      id="interest"
+                      onChange={formik.handleChange}
+                      value={formik.values.interest}
+                      isInvalid={!!formik.errors.interest && formik.touched.interest}
+                    />
 
-                                <Form.Control.Feedback type="invalid">
-                                    {formik.errors.company}
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                        </Col>
-                        <Col md={6}>
+                    <Form.Control.Feedback type="invalid">
+                      {formik.errors.interest}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={12}>
+                  <Form.Group>
+                    <Form.Control
+                      className="form-control formfield textarea"
+                      placeholder={props.labelMessage[lang]}
+                      name="message"
+                      id="message"
+                      onChange={formik.handleChange}
+                      value={formik.values.message}
+                      as="textarea"
+                      isInvalid={!!formik.errors.message && formik.touched.message}
+                    />
 
-                           <Form.Group>
-                                <Form.Control
-                                    type="text"
-                                    className="form-control formfield"
-                                    placeholder={props.labelInterest[lang]}
-                                    name="interest"
-                                    id="interest"
-                                    onChange={formik.handleChange}
-                                    value={formik.values.interest}
-                                    isInvalid={!!formik.errors.interest && formik.touched.interest} />
-
-                                <Form.Control.Feedback type="invalid">
-                                    {formik.errors.interest}
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md={12}>
-                         <Form.Group>
-                              <Form.Control
-                                  className="form-control formfield textarea"
-                                  placeholder={props.labelMessage[lang]}
-                                  name="message"
-                                  id="message"
-                                  onChange={formik.handleChange}
-                                  value={formik.values.message}
-                                  as="textarea"
-                                  isInvalid={!!formik.errors.message && formik.touched.message} />
-
-                              <Form.Control.Feedback type="invalid">
-                                  {formik.errors.message}
-                              </Form.Control.Feedback>
-                          </Form.Group>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md={12}>
-                        <div style={{textAlign:'left',marginTop:'50px'}}>
-                            <button type="submit" className='Btnformmail' > SUBMIT FORM</button>
-                          </div>  
-                          <div id="output"></div>
-                        </Col>
-                    </Row>                    
-                </form>
-
-              </Col>
-          </Row>
+                    <Form.Control.Feedback type="invalid">
+                      {formik.errors.message}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={12}>
+                  <div style={{ textAlign: "left", marginTop: "50px" }}>
+                    <button type="submit" className="Btnformmail">
+                      {" "}
+                      SUBMIT FORM
+                    </button>
+                  </div>
+                  <div id="output"></div>
+                </Col>
+              </Row>
+            </form>
+          </Col>
+        </Row>
       </div>
     </section>
   );
